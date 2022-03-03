9239c59fea9187e11602aaa4394b55a3cc460b5c
uchan-nos <uchan@uchan.net>
2022-03-03 15:58:54 +0900

    コンパイラを +, -, * の演算に対応させる

--- workshop/chap4/compiler/main.c
+++ workshop/chap4/compiler/main.c
@@ -18,15 +18,47 @@ int scan_char() {
   return c;
 }
 
+int consume_char(int c) {
+  int scanned = scan_char();
+  if (scanned == c) {
+    return c;
+  }
+  ungetc(scanned, stdin);
+  return 0;
+}
+
+void Additive();
+void Multiplicative();
+void Primary();
+
+void Additive() {
+  Multiplicative();
+
+  if (consume_char('+')) {
+    Additive();
+    printf("0300\n");
+  } else if (consume_char('-')) {
+    Additive();
+    printf("0400\n");
+  }
+}
+
+void Multiplicative() {
+  Primary();
+
+  if (consume_char('*')) {
+    Multiplicative();
+    printf("0500\n");
+  }
+}
+
+void Primary() {
+  int v = scan_int();
+  printf("01%02x\n", (uint8_t)v);
+}
+
 int main(void) {
   char c;
-  printf("01%02x\n", scan_int());
-  while ((c = scan_char()) != '\0') {
-    if (c == '+') {
-      printf("03%02x\n", (uint8_t)scan_int());
-    } else if (c == '-') {
-      printf("03%02x\n", (uint8_t)-scan_int());
-    }
-  }
+  Additive();
   printf("0200\n");
 }
