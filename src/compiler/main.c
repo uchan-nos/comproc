cbbf435d6ac6189a7e51d9f810e4d348352a52da
uchan-nos <uchan@uchan.net>
2022-03-09 11:00:21 +0900

    chap4 を src にコピー

---
+++ src/compiler/main.c
@@ -0,0 +1,63 @@
+#include <ctype.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int scan_int() {
+  int x = 0;
+  scanf("%d", &x);
+  return x;
+}
+
+int scan_char() {
+  char c = '\0';
+  while (scanf("%c", &c) == 1) {
+    if (!isspace(c)) {
+      break;
+    }
+  }
+  return c;
+}
+
+int consume_char(int c) {
+  int scanned = scan_char();
+  if (scanned == c) {
+    return c;
+  }
+  ungetc(scanned, stdin);
+  return 0;
+}
+
+void Additive();
+void Multiplicative();
+void Primary();
+
+void Additive() {
+  Multiplicative();
+
+  if (consume_char('+')) {
+    Additive();
+    printf("0300\n");
+  } else if (consume_char('-')) {
+    Additive();
+    printf("0400\n");
+  }
+}
+
+void Multiplicative() {
+  Primary();
+
+  if (consume_char('*')) {
+    Multiplicative();
+    printf("0500\n");
+  }
+}
+
+void Primary() {
+  int v = scan_int();
+  printf("01%02x\n", (uint8_t)v);
+}
+
+int main(void) {
+  Additive();
+  printf("0200\n");
+}
---
+++ src/cpu/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -eu
+
+target=${TARGET:-sim}
+uart_dev="/dev/ttyUSB0"
+
+function test_value() {
+  want="$1"
+  input="$2"
+
+  case $target in
+    sim)
+      got=$(echo $input | ./sim.exe 2>&1 1>/dev/null)
+      ;;
+    uart)
+      got=$(echo $(sudo ../../../tool/uart.py --dev "$uart_dev" --unit 2 $input))
+      ;;
+    *)
+      echo "unknown target: $target"
+      exit 1
+      ;;
+  esac
+
+  if [ $((0x$want)) = $((0x$got)) ]
+  then
+    echo "[  OK  ]: $input -> '$got'"
+  else
+    echo "[FAILED]: $input -> '$got', want '$want'"
+  fi
+}
+
+test_value 1f "011f 0200 ffff"
+test_value fe "0101 0103 0400 0200 ffff"
+test_value 07 "0101 0102 0103 0500 0300 0200 ffff"
