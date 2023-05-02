41adb27f262b340c04d22c9c8036df9a8655e3cf
uchan-nos <uchan@uchan.net>
2023-05-03 08:57:20 +0900

    カウントダウンタイマのモジュールを追加、テスト改良

---
+++ src/cpu/cdtimer.sv
@@ -0,0 +1,53 @@
+`include "common.sv"
+
+/*
+カウントダウンタイマ
+
+書き込まれた初期値を PERIOD クロック毎にカウントダウンする。
+（PERIOD は 0 <= PERIOD <= 65535 で指定可能）
+カウンタが 0 になったらカウントダウン動作を終了する。
+
+load=1 かつ clk の立ち上がりで data を初期値として取り込む。
+*/
+
+module cdtimer#(
+  parameter PERIOD = 27_000,
+  parameter WIDTH = 16
+) (
+  input rst,
+  input clk,
+  input load,
+  input [WIDTH-1:0] data,
+  output logic [WIDTH-1:0] counter,
+  output logic timeout
+);
+
+// カウントダウン周期生成用
+logic [15:0] period, inc;
+assign inc = period + 16'd1;
+
+always @(posedge clk, posedge rst) begin
+  if (rst) begin
+    counter <= 0;
+    period <= 0;
+    timeout <= 0;
+  end else if (load) begin
+    counter <= data;
+    period <= 0;
+    timeout <= 0;
+  end else if (timeout) begin
+    // pass
+  end else if (inc < PERIOD) begin
+    period <= inc;
+  end else begin
+    period <= 0;
+    if (counter > 1)
+      counter <= counter - 1;
+    else begin
+      counter <= 0;
+      timeout <= 1;
+    end
+  end
+end
+
+endmodule
---
+++ src/cpu/cdtimer_tb.sv
@@ -0,0 +1,117 @@
+module cdtimer_tb;
+
+logic rst, clk, load, timeout;
+logic [3:0] data, counter;
+
+cdtimer#(.PERIOD(3), .WIDTH(4)) cdtimer(.*);
+
+// 10 単位時間周期のクロックを生成
+always #5 begin
+  clk <= ~clk;
+end
+
+integer i, j;
+
+initial begin
+  $monitor("%d: rst=%d load=%d data=%d counter=%d timeout=%d period=%d",
+           $time, rst, load, data, counter, timeout, cdtimer.period);
+
+  rst <= 1;
+  clk <= 1;
+  load <= 1;
+  data <= 4'd2;
+
+  #13
+    rst <= 0;
+    test_timer(4'd0, 0, 16'd0);
+
+  @(posedge clk)
+    load <= 0;
+  @(negedge clk)
+    test_timer(4'd2, 0, 16'd0);
+
+  @(posedge clk)
+  @(negedge clk)
+    test_timer(4'd2, 0, 16'd1);
+
+  @(posedge clk)
+  @(negedge clk)
+    test_timer(4'd2, 0, 16'd2);
+
+  @(posedge clk)
+  @(negedge clk)
+    test_timer(4'd1, 0, 16'd0);
+
+  @(posedge clk)
+  @(negedge clk)
+    test_timer(4'd1, 0, 16'd1);
+
+  @(posedge clk)
+  @(negedge clk)
+    test_timer(4'd1, 0, 16'd2);
+
+  @(posedge clk)
+  @(negedge clk)
+    test_timer(4'd0, 1, 16'd0);
+
+  @(posedge clk)
+  @(negedge clk)
+    test_timer(4'd0, 1, 16'd0);
+
+  @(posedge clk)
+    load <= 1;
+    data <= 4'd5;
+  @(negedge clk)
+    test_timer(4'd0, 1, 16'd0);
+
+  @(posedge clk)
+    load <= 0;
+  @(negedge clk)
+    test_timer(4'd5, 0, 16'd0);
+
+  @(posedge clk)
+    load <= 1;
+    data <= 4'd10;
+  @(negedge clk)
+    test_timer(4'd5, 0, 16'd1);
+
+  @(posedge clk)
+    load <= 0;
+  @(negedge clk)
+    test_timer(4'd10, 0, 16'd0);
+
+  @(posedge clk)
+  @(negedge clk)
+    test_timer(4'd10, 0, 16'd1);
+
+  @(posedge clk)
+  @(negedge clk)
+    test_timer(4'd10, 0, 16'd2);
+
+  for (i = 9; i > 0; i = i - 1) begin
+    for (j = 0; j < 3; j = j + 1) begin
+      @(posedge clk)
+      @(negedge clk)
+        test_timer(i, 0, j);
+    end
+  end
+
+  @(posedge clk)
+  @(negedge clk)
+    test_timer(4'd0, 1, 16'd0);
+
+  $finish;
+end
+
+task test_timer(input [3:0] e_counter, input e_timeout, input [15:0] e_period);
+begin
+  if (e_counter !== 4'hx && counter !== e_counter)
+    $error("counter must be %d", e_counter);
+  if (e_timeout !== 1'bx && timeout !== e_timeout)
+    $error("timeout must be %d", e_timeout);
+  if (e_period !== 16'hxxxx && cdtimer.period !== e_period)
+    $error("period must be %d", e_period);
+end
+endtask
+
+endmodule
