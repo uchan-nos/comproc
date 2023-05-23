cbf4f7e4610127cb54b572ea60b4d9b5237a4f6e
uchan-nos <uchan@uchan.net>
2023-05-23 11:14:47 +0900

    タイミング図を更新

--- doc/signal-timing-design/cpu-mem-timing.py
+++ doc/signal-timing-design/cpu-mem-timing.py
@@ -3,26 +3,26 @@
     {name: 'clk',          wave: 'PPPPPPPPPPPPPPPPPP'},
     {name: 'phase',        wave: '222222222222222222', data: ['R', 'F', 'D', 'E', 'R', 'F', 'D', 'E', 'R', 'F', 'D', 'E', 'R', 'F', 'D', 'E', 'R', 'F']},
     {name: 'ip',           wave: '3.4...5...6...7...', data: ['X', 'X+2', 'X+4', 'X+6', 'X+8']},
-    {name: 'load_ip',      wave: '010..10..10..10..1'}, // phase_fetch | reload_ip
+    {name: 'load_ip',      wave: '010..10..10..10..1'},
     {name: 'src_a',        wave: '2.2.2.2.2.2.2.2.2.', data: ['ip', 'cstack', 'ip', 'stack0', 'ip', 'stack0', 'ip', 'stack0', 'ip']},
     {name: 'src_b',        wave: 'x.2...2...........', data: ['imm', 'stack1']},
   	{name: 'alu_sel',      wave: '22x222x222x222x222', data: ['A', 'INC2', 'ADD', 'A', 'INC2', 'A', 'A', 'INC2', 'A', 'A', 'INC2', 'ADD', 'A', 'INC2']},
     {name: 'alu_out',      wave: '34x245x256x267x278', data: ['X', 'X+2', 'T+Y', 'X+2', 'X+4', 'M0', 'X+4', 'X+6', 'M0', 'X+6', 'X+8', 'S2', 'X+8']},
     {name: 'rd_data',      wave: 'x34x245x256x267x.7', data: ['ins1', 'ins2', 'M0', 'ins2', 'ins3', 'S0', 'ins3', 'ins4', 'S0', 'ins4', 'ins5', 'ins5']},
-    {name: 'wr_mem',       wave: '0......10.........'}, // insn_wrmem & phase_exec
+    {name: 'wr_mem',       wave: '0......10.........'},
     {name: 'wr_stk1',      wave: 'x.0...1.......0...'},
     {name: 'insn',         wave: 'x.3...4...5...6...', data: ['ins1: LD cstack+Y', 'ins2: STA', 'ins3: LDD', 'ins4: ADD']},
     {name: 'stack0',       wave: '2....2..2....2..2.', data: ['S0', 'M0', 'M0', 'S0', 'S2=S0+S1']},
     {name: 'stack1',       wave: '2....2..2.......x.', data: ['S1', 'S0', 'S1']},
     {name: 'pop',          wave: '0......10......10.'},
     {name: 'push',         wave: 'x0..10............'},
-    {name: 'load_stk',     wave: '0...10.10...10.10.'}, // (~insn_rdmem & phase_exec) | (insn_rdmem & phase_rdmem)
+    {name: 'load_stk',     wave: '0...10.10...10.10.'},
     {name: 'fp',           wave: 'x.................'},
-    {name: 'load_fp',      wave: '0.................'}, // phase_fetch | reload_ip
+    {name: 'load_fp',      wave: '0.................'},
     {name: 'cstack0',      wave: '2.................', data: ['T']},
     {name: 'cpop',         wave: '0.................'},
     {name: 'cpush',        wave: '0.................'},
     {name: 'insn_src_a',   wave: 'x.2...2...........', data: ['cstack', 'stack0']},
-    {name: 'src_a_ip',     wave: '1.0.1.0.1.0.1.0.1.'}, // ~phase_half | insn_src_a === 2
+    {name: 'src_a_ip',     wave: '1.0.1.0.1.0.1.0.1.'},
   ],
 }
--- doc/signal-timing-design/cpu-mem-timing_jz-fp.py
+++ doc/signal-timing-design/cpu-mem-timing_jz-fp.py
@@ -2,30 +2,25 @@
   signal: [
     {name: 'clk',          wave: 'PPPPPPPPPPPPPPPPPP'},
     {name: 'phase',        wave: '222222222222222222', data: [2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]},
-    {name: 'ip',           wave: '3.4...2.5.6...7...', data: ['X', 'X+2', 'X+4', 'Y', 'Y+2', 'Y+4']},
-    {name: 'load_ip',      wave: '01010101010..10..1'}, // phase_fetch | reload_ip
-    {name: 'Source A',     wave: '2.2.2.2.2.2.2.2.2.', data: ['ip', 'ip', 'ip', 'ip', 'ip', 'fp', 'ip', 'cstack[0]', 'ip']},
-    {name: 'Source B',     wave: 'x.2...............', data: ['imm']},
-  	{name: 'ALU',          wave: '22x222x222x222x222', data: ['A', 'A+2', 'A:B', 'A', 'A+2', 'A:B', 'A', 'A+2', 'A',   'A', 'A+2', 'A', 'A', 'A+2']},
-    {name: 'alu_out',      wave: '34x4.2x5.6x267x272', data: ['X', 'X+2', 'X+2', 'X+4', 'Y', 'Y+2', 'S0', 'Y+2', 'Y+4', 'S0', 'Y+4', 'Y+6']},
-    {name: 'mem_out',      wave: 'x34x4.x.5.6x.67x.7', data: ['ins1', 'ins2', 'ins2', 'ins3', 'ins4', 'ins4', 'ins5', 'ins5']},
-    {name: 'insn',         wave: 'x.3...4...5...6...', data: ['ins1: JZ Y', 'ins2: JZ Y', 'ins3: PUSHFP', 'ins4: POPFP']},
-    {name: 'insn_ip',      wave: 'x.1.......0.......',}, // ip を用いる命令
-    {name: 'insn_fp',      wave: 'x.0.......1.......',}, // fp を用いる命令
-    {name: 'stack[0]',     wave: '2....2....x.......', data: ['1', '0']},
-    {name: 'stack[1]',     wave: '2....x............', data: ['0']},
-    {name: 'insn_cpop',    wave: 'x.0...........1...'},
-    {name: 'insn_cpush',   wave: 'x.0.......1...0...'},
-    {name: 'fp',           wave: '2...............2.', data: ['S0', 'S0']},
-    {name: 'cstack[0]',    wave: 'x...........2...x.', data: ['S0']},
-    {name: 'phase_decode', wave: '0.10..10..10..10..'}, // phase == 0
-    {name: 'phase_exec',   wave: '0..10..10..10..10.'}, // phase == 1
-    {name: 'phase_rdmem',  wave: '10..10..10..10..10'}, // phase == 2
-    {name: 'phase_fetch',  wave: '010..10..10..10..1'}, // phase == 2
-    {name: 'reload_ip',    wave: '0..10..10.........'}, // insn_br & phase_exec
-    {name: 'phase_half',   wave: '0.1.0.1.0.1.0.1.0.'}, // phase_decode | phase_exec
-    {name: 'sel_a_ip',     wave: '1.........0.1.0.1.'}, // insn_ip | ~phase_half
-    {name: 'sel_a_fp',     wave: '0.........1.0.....'}, // insn_cpush & insn_fp & phase_half
-    {name: 'sel_a_cstack', wave: '0.............1.0.'}, // insn_cpop & phase_half
+    {name: 'ip',           wave: '3.4...2.5.6...7...', data: ['X', 'X+2', 'X+4', 'X+8', 'X+10', 'X+12']},
+    {name: 'load_ip',      wave: '01010101010..10..1'},
+    {name: 'src_a',        wave: '2.2.2.2.2.2.2.2.2.', data: ['ip', 'ip', 'ip', 'ip', 'ip', 'fp', 'ip', 'cstack', 'ip']},
+    {name: 'src_b',        wave: 'x.2.......2.......', data: ['imm', 'stack1']},
+  	{name: 'alu_sel',      wave: '22x222x2222.22x222', data: ['A', 'A+2', 'ADDZ', 'A', 'A+2', 'ADDZ', 'A', 'A+2', 'A',   'A', 'A+2', 'A', 'A', 'A+2']},
+    {name: 'alu_out',      wave: '34x4.2x5.62.67x272', data: ['X', 'X+2', 'X+2', 'X+4', 'X+8', 'X+10', 'S', 'X+10', 'X+12', 'S', 'X+12', 'X+14']},
+    {name: 'rd_data',      wave: 'x34x4.x.5.6x.67x.7', data: ['ins1', 'ins2', 'ins2', 'ins3', 'ins4', 'ins4', 'ins5', 'ins5']},
+    {name: 'insn',         wave: 'x.3...4...5...6...', data: ['ins1: JZ 6', 'ins2: JZ 4', 'ins3: CPUSH FP', 'ins4: CPOP FP']},
+    {name: 'stack0',       wave: '2...2...x.........', data: ['1', '0']},
+    {name: 'stack1',       wave: '2...x.............', data: ['0']},
+    {name: 'pop',          wave: '0..10..10.........'},
+    {name: 'push',         wave: 'x0................'},
+    {name: 'load_stk',     wave: '0.................'},
+    {name: 'fp',           wave: '2...............2.', data: ['S', 'S']},
+    {name: 'load_fp',      wave: '0..............10.'},
+    {name: 'cstack0',      wave: 'x..........2....x.', data: ['S']},
+    {name: 'cpop',         wave: '0..............10.'},
+    {name: 'cpush',        wave: '0.........10......'},
+    {name: 'insn_src_a',   wave: 'x.2.......2...2...', data: ['ip', 'fp', 'cstack']},
+    {name: 'src_a_ip',     wave: '1.........0.1.0.1.'},
   ],
 }
