ce641465dadc41dfabaf88df55ba180e05d6b27b
uchan-nos <uchan@uchan.net>
2023-08-02 06:13:15 +0900

    フレーム毎にSVGファイルを描画

--- src/cpu/visualizer/vis.py
+++ src/cpu/visualizer/vis.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from collections import namedtuple
-import drawsvg
+import drawsvg as dw
 
 
 # Types
@@ -48,16 +48,42 @@ def parse_trace_line(line):
     return Trace(int(parts[0]), values)
 
 
+def draw_alu(**args):
+    g = dw.Group(id='alu', fill='none', stroke='black', **args)
+    p = dw.Path()
+    p.M(0, 0).L(0, 15).L(10, 20).L(0, 25).L(0, 40).L(20, 30).L(20, 10).Z()
+    g.append(p)
+    g.append(dw.Text('A', 10, 3, 13))
+    g.append(dw.Text('B', 10, 3, 33))
+    return g
+
+
+def draw_stack(stack, **args):
+    g = dw.Group(id='stack', **args)
+    for i in range(16):
+        g.append(dw.Rectangle(0, i*16, 60, 16, fill='none', stroke='black'))
+        g.append(dw.Text(stack[i], 16, 60//2, i*16 + 16//2, center=True, font_family='Consolas'))
+    return g
+
+
 def main():
-    d = drawsvg.Drawing(600, 400)
 
     cpu = CPU()
-    with open('../trace.txt') as trace_file:
+    with open('trace.txt') as trace_file:
         for line in trace_file:
             t = parse_trace_line(line)
             print(t.time, t.values['stack0'], cpu.stack[:2], t.values['cstack0'], cpu.cstack[:2])
-            cpu.clk(t)
 
+            d = dw.Drawing(600, 400)
+            d.append(dw.Rectangle(0, 0, d.width, d.height, fill='white'))
+            d.append(dw.Text('ComProc CPU Visualizer', 12, 5, 15))
+            d.append(dw.Rectangle(5, 20, d.width - 10, d.height - 25, fill='none', stroke='gray', stroke_width=1))
+            d.append(draw_alu(transform=f'translate({t.time}, 50)'))
+            d.append(draw_stack(cpu.stack, transform='translate(10, 30)'))
+            d.save_svg(f'vis-{t.time:06}.svg')
+
+            cpu.clk(t)
+ 
 
 if __name__ == '__main__':
     main()
