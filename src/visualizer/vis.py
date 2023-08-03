e0db261b1100fdac70e5f43f267642fdb32a7d60
uchan-nos <uchan@uchan.net>
2023-08-03 14:34:15 +0900

    MUX やバスの信号値を追加

--- src/visualizer/vis.py
+++ src/visualizer/vis.py
@@ -56,18 +56,43 @@ def parse_trace_line(line):
 
 
 def draw_alu(**args):
-    g = dw.Group(id='alu', fill='none', stroke='black', **args)
-    p = dw.Path()
-    p.M(0, 0).L(0, 15).L(10, 20).L(0, 25).L(0, 40).L(20, 30).L(20, 10).Z()
+    g = dw.Group(id='alu', **args)
+    p = dw.Path(fill='none', stroke='black')
+    p.M(0, 0).L(0, 24).L(16, 32).L(0, 40).L(0, 64).L(30, 49).L(30, 15).Z()
     g.append(p)
-    g.append(dw.Text('A', 10, 3, 13))
-    g.append(dw.Text('B', 10, 3, 33))
+    g.append(dw.Text('A', 12, 3, 16, dominant_baseline='middle'))
+    g.append(dw.Text('B', 12, 3, 48, dominant_baseline='middle'))
     return g
 
 
-def draw_stack(stack, **args):
-    g = dw.Group(id='stack', **args)
-    for i in range(16):
+def draw_mux2(**args):
+    g = dw.Group(id='alu', **args)
+    p = dw.Path(fill='none', stroke='black')
+    p.M(0, 0).L(0, 64).L(30, 64-15).L(30, 15).Z()
+    g.append(p)
+    g.append(dw.Text('0', 12, 3, 16 + 16*0, dominant_baseline='middle'))
+    g.append(dw.Text('1', 12, 3, 16 + 16*2, dominant_baseline='middle'))
+    g.append(dw.Text('O', 12, 27, 64//2, text_anchor='end', dominant_baseline='middle'))
+    return g
+
+
+def draw_mux4(**args):
+    g = dw.Group(id='alu', **args)
+    p = dw.Path(fill='none', stroke='black')
+    p.M(0, 0).L(0, 80).L(30, 65).L(30, 15).Z()
+    g.append(p)
+    g.append(dw.Text('0', 12, 3, 16 + 16*0, dominant_baseline='middle'))
+    g.append(dw.Text('1', 12, 3, 16 + 16*1, dominant_baseline='middle'))
+    g.append(dw.Text('2', 12, 3, 16 + 16*2, dominant_baseline='middle'))
+    g.append(dw.Text('3', 12, 3, 16 + 16*3, dominant_baseline='middle'))
+    g.append(dw.Text('O', 12, 27, 80//2, text_anchor='end', dominant_baseline='middle'))
+    return g
+
+
+def draw_stack(name, stack, depth, **args):
+    g = dw.Group(id=name, **args)
+    g.append(dw.Text(name, 16, 60//2, -5, text_anchor='middle'))
+    for i in range(depth):
         g.append(dw.Rectangle(0, i*16, 60, 16, fill='none', stroke='black'))
         g.append(dw.Text(stack[i], 16, 60//2, i*16 + 16//2, center=True, font_family='Consolas'))
     return g
@@ -75,7 +100,7 @@ def draw_stack(stack, **args):
 
 def draw_reg(name, value, **args):
     g = dw.Group(id='reg-' + name, **args)
-    g.append(dw.Text(name, 16, -5, 8, text_anchor='end', dominant_baseline='middle'))
+    g.append(dw.Text(name, 16, 60//2, -5, text_anchor='middle'))
     g.append(dw.Rectangle(0, 0, 60, 16, fill='none', stroke='black'))
     g.append(dw.Text(value, 16, 60//2, 16//2, center=True, font_family='Consolas'))
     return g
@@ -101,18 +126,75 @@ def gen_frames():
 
             t = parse_trace_line(line)
 
-            d = dw.Drawing(600, 400)
+            stack_x = 150
+            stack_y = 100
+            fp_y = 100+16*4
+            ip_y = 100+16*7
+            cstk_y = 100+16*10
+            src_a_x = 350
+            src_a_y = 100
+            src_b_y = 220
+            alu_x = 450
+            insn_y = 500
+            d = dw.Drawing(800, 600)
             d.append(dw.Rectangle(0, 0, d.width, d.height, fill='white'))
             d.append(dw.Text('ComProc CPU Visualizer', 12, 5, 15))
             d.append(dw.Rectangle(5, 20, d.width - 10, d.height - 25, fill='none', stroke='gray', stroke_width=1))
-            d.append(draw_alu(transform=f'translate(300, 50)'))
-            d.append(draw_stack(cpu.stack, transform='translate(150, 50)'))
+            d.append(draw_alu(transform=f'translate({alu_x}, {src_a_y+40-16})'))
             append_text = gen_text_appender(d)
             append_text('Reset: ' + ('enable' if t.values['rst'] == '1' else 'disable'))
             append_text('Phase: ' + phase_names[t.values['phase']])
-            d.append(draw_reg('fp', t.values['fp'], transform='translate(50, 200)'))
-            d.append(draw_reg('ip', t.values['ip'], transform='translate(50, 220)'))
-            d.append(draw_reg('insn', t.values['insn'], transform='translate(50, 240)'))
+            d.append(draw_stack('stack', cpu.stack, 2, transform=f'translate({stack_x}, {stack_y})'))
+            d.append(draw_reg('fp', t.values['fp'], transform=f'translate({stack_x}, {fp_y})'))
+            d.append(draw_reg('ip', t.values['ip'], transform=f'translate({stack_x}, {ip_y})'))
+            d.append(draw_stack('cstack', cpu.cstack, 2, transform=f'translate({stack_x}, {cstk_y})'))
+            d.append(draw_reg('insn', t.values['insn'], transform=f'translate({stack_x}, {insn_y})'))
+            d.append(draw_mux4(transform=f'translate({src_a_x}, {src_a_y})'))
+            d.append(draw_mux2(transform=f'translate({src_a_x}, {src_b_y})'))
+            d.append(draw_stack('stack', cpu.stack, 16, transform=f'translate(630, 300)'))
+            d.append(draw_stack('cstack', cpu.cstack, 16, transform=f'translate(700, 300)'))
+
+            line_args = {'fill': 'none'}
+            src_a_stroke = lambda i: 'red' if t.values['src_a_sel'] == str(i) else 'black'
+            d.append(dw.Path(stroke=src_a_stroke(0), **line_args).M(stack_x+60, stack_y+8)
+                     .H(stack_x+80).V(src_a_y+16).H(src_a_x))
+            d.append(dw.Path(stroke=src_a_stroke(1), **line_args).M(stack_x+60, fp_y+8)
+                     .H(stack_x+80).V(src_a_y+32).H(src_a_x))
+            d.append(dw.Path(stroke=src_a_stroke(2), **line_args).M(stack_x+60, ip_y+8)
+                     .H(stack_x+90).V(src_a_y+48).H(src_a_x))
+            d.append(dw.Path(stroke=src_a_stroke(3), **line_args).M(stack_x+60, cstk_y+8)
+                     .H(stack_x+100).V(src_a_y+64).H(src_a_x))
+
+            src_b_stroke = lambda i: 'red' if t.values['imm'] == str(i) else 'black'
+            d.append(dw.Path(stroke=src_b_stroke(0), **line_args).M(stack_x+60, stack_y+16+8)
+                     .H(stack_x+140).V(src_b_y+16).H(src_a_x))
+            d.append(dw.Path(stroke=src_b_stroke(1), **line_args).M(stack_x+60, insn_y+8)
+                     .H(stack_x+140).V(src_b_y+48).H(src_a_x))
+
+            try:
+                alu_sel = int(t.values['alu_sel'], 16)
+            except ValueError:
+                alu_sel = -1
+            src_a_out_stroke = 'red' if 0 <= alu_sel <= 4 or 16 <= alu_sel else 'black'
+            src_b_out_stroke = 'red' if 15 <= alu_sel else 'black'
+            d.append(dw.Path(stroke=src_a_out_stroke, **line_args).M(src_a_x+30, src_a_y+40).H(alu_x))
+            d.append(dw.Path(stroke=src_b_out_stroke, **line_args).M(src_a_x+30, src_b_y+32)
+                     .H(src_a_x+60).V(src_a_y+40+32).H(alu_x))
+
+            line_args['stroke'] = 'black'
+            alu_sel_name = alu_sel_names.get(t.values['alu_sel'], '??')
+            d.append(dw.Path(**line_args).M(alu_x+15, src_a_y+120).V(src_a_y+40-16+64-7.5))
+            d.append(dw.Text(t.values['alu_sel'], 16, alu_x+15, src_a_y+120+16,
+                             text_anchor='end', font_family='Consolas'))
+            d.append(dw.Text(f'({alu_sel_name})', 16, alu_x+20, src_a_y+120+16))
+            d.append(dw.Path(**line_args).M(alu_x+30, src_a_y+40-16+32).H(alu_x+150))
+            d.append(dw.Text('alu_out=', 16, alu_x+120, src_a_y+40-16+32-5, text_anchor='end'))
+            d.append(dw.Text(t.values['alu_out'], 16, alu_x+120, src_a_y+40-16+32-5, font_family='Consolas'))
+
+            d.append(dw.Path(**line_args).M(src_a_x+15, src_b_y+80).V(src_b_y+64-7.5))
+            d.append(dw.Text('imm=', 16, src_a_x+30, src_b_y+80+16, text_anchor='end'))
+            d.append(dw.Text(t.values['imm'], 16, src_a_x+30, src_b_y+80+16, font_family='Consolas'))
+
             d.save_svg(f'vis-{i}.svg')
 
             cpu.clk(t)
@@ -132,6 +214,29 @@ phase_names = {
     '2': 'RDMem',
     '3': 'Fetch',
     'x': '----'
+}
+alu_sel_names = {
+    '00': 'A',
+    '01': 'INC',
+    '02': 'INC2',
+    '03': 'INC3',
+    '04': 'NOT',
+    '0f': 'B',
+    '10': 'AND',
+    '11': 'OR',
+    '12': 'XOR',
+    '14': 'SHR',
+    '15': 'SAR',
+    '16': 'SHL',
+    '17': 'JOIN',
+    '20': 'ADD',
+    '21': 'SUB',
+    '22': 'MUL',
+    '28': 'LT',
+    '29': 'EQ',
+    '2a': 'NEQ',
+    '30': 'ADDZ',
+    '31': 'ADDNZ',
 }
 
 
