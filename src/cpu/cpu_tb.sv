cbd828da7a86ad6105930f4d0762592a7e8057f8
uchan-nos <uchan@uchan.net>
2023-10-12 14:50:00 +0900

    ユニットテスト test_modules をパスするよう修正

--- src/cpu/cpu_tb.sv
+++ src/cpu/cpu_tb.sv
@@ -24,6 +24,7 @@ initial begin
   rst <= 1;
   clk <= 1;
   rd_data <= 16'h80A5; // PUSH 0xA5
+  irq <= 0;
 
   #1 rst <= 0;
 
@@ -35,21 +36,21 @@ initial begin
     if (~cpu.load_insn) $error("load_insn must be 1");
 
   @(posedge cpu.load_insn)
-    rd_data <= 16'h7004; // NOT
+    rd_data <= 16'h7004; // 300: NOT
     if (cpu.stack0 !== 16'h00A5) $error("stack0 must be 0x00A5");
 
   @(posedge cpu.load_insn)
-    rd_data <= 16'h8001; // PUSH 0x01
+    rd_data <= 16'h8001; // 302: PUSH 0x01
     if (cpu.stack0 !== 16'hFF5A) $error("stack0 must be 0xFF5A");
 
   @(posedge cpu.load_insn)
-    rd_data <= 16'h0FFE; // JMP IP-2
+    rd_data <= 16'h0FFE; // 304: JMP IP-2
 
   #11 // decode
   #10 // exec
     if (cpu.alu_out !== 16'h306) $error("alu_out must be 0x306");
   #10 // rdmem
-    rd_data <= 16'h1020; // JZ 0x20
+    rd_data <= 16'h1020; // 306: JZ 0x20
   @(posedge cpu.load_insn)
     if (cpu.stack0 !== 16'h0001) $error("stack0 must be 0x0001");
     if (cpu.stack1 !== 16'hFF5A) $error("stack1 must be 0xFF5A");
@@ -60,7 +61,7 @@ initial begin
   #10 // rdmem
     if (~cpu.signals.phase_rdmem) $error("phase_rdmem must be 1");
     if (mem_addr !== `ADDR_WIDTH'h308) $error("mem_addr must be 0x308");
-    rd_data <= 16'h1030; // JZ 0x30
+    rd_data <= 16'h1030; // 308: JZ 0x30
   @(posedge cpu.load_insn)
 
   #11 // decode
@@ -69,7 +70,7 @@ initial begin
   #10 // rdmem
     if (~cpu.signals.phase_rdmem) $error("phase_rdmem must be 1");
     if (mem_addr !== `ADDR_WIDTH'h33A) $error("mem_addr must be 0x33A");
-    rd_data <= 16'h2103; // LD.1 0x103
+    rd_data <= 16'h2103; // 33a: LD.1 0x103
   @(posedge cpu.load_insn)
 
   #11 // decode
@@ -78,28 +79,26 @@ initial begin
   #10 // rdmem
     rd_data <= 16'hDEAD;
   @(posedge cpu.load_insn)
-    rd_data <= 16'h7001; // INC
+    rd_data <= 16'h7001; // 33c: INC
     if (cpu.stack0 !== 16'h00DE) $error("stack0 must be 0x00DE");
 
   @(posedge cpu.load_insn)
-    rd_data <= 16'h4005; // ST 0+4
+    rd_data <= 16'h4005; // 33e: ST 0+4
     if (cpu.stack0 !== 16'h00DF) $error("stack0 must be 0x00DF");
 
   @(posedge cpu.load_insn)
+    rd_data <= 16'h8012; // 340: PUSH 0x12
     if (last_wr_addr !== 16'h0004) $error("last_wr_addr must be 0x0004");
     if (last_wr_data !== 16'h00DF) $error("last_wr_data must be 0x00DF");
 
   @(posedge cpu.load_insn)
-    rd_data <= 16'h8012; // PUSH 0x12
+    rd_data <= 16'h8081; // 342: PUSH 0x81
 
   @(posedge cpu.load_insn)
-    rd_data <= 16'h8081; // PUSH 0x81
+    rd_data <= 16'h780D; // 344: STA.1
 
   @(posedge cpu.load_insn)
-    rd_data <= 16'h780D; // STA.1
-
-  @(posedge cpu.load_insn)
-    rd_data <= 16'h7809; // LDD.1
+    rd_data <= 16'h7809; // 346: LDD.1
     if (cpu.stack0 !== 16'h0081) $error("stack0 must be 0x0081");
     if (last_wr_addr !== 16'h0081) $error("last_wr_addr must be 0x0081");
     if (last_wr_data !== 16'h1200) $error("last_wr_data must be 0x1200");
@@ -110,57 +109,42 @@ initial begin
   #10 // rdmem
     rd_data <= 16'h1200;
   @(posedge cpu.load_insn)
-    rd_data <= 16'h80FF; // PUSH 0xFF
+    rd_data <= 16'h80FF; // 348: PUSH 0xFF
     if (cpu.stack0 !== 16'h0012) $error("stack0 must be 0x0012");
 
   @(posedge cpu.load_insn)
-    rd_data <= 16'h8900; // PUSH 0x900
+    rd_data <= 16'h8900; // 34a: PUSH 0x900
 
   @(posedge cpu.load_insn)
-    rd_data <= 16'h7811; // ISR
-
-  /*
-  @(posedge cpu.load_insn)
-    rd_data <= 16'h8002; // PUSH 2
+    rd_data <= 16'h7811; // 34c: ISR
 
   @(posedge cpu.load_insn)
-    rd_data <= 16'h4005; // ST 4  (mem[4] <= 2: enable interrupt of cdtimer)
-
-  @(posedge cpu.load_insn)
-    rd_data <= 16'h8001; // PUSH 1
-
-  @(posedge cpu.load_insn)
-    rd_data <= 16'h4003; // ST 2  (mem[2] <= 1: write 1 to counter of cdtimer)
+    rd_data <= 16'h80FE; // 34e: PUSH 0xFE
+    irq <= 1;
 
   #11 // decode
   #10 // exec
   #10 // rdmem
-    if (cpu.cdtimer_to !== 1'b0) $error("cdtimer must NOT be timed out");
-
-  @(posedge cpu.load_insn)
-    rd_data <= 16'h80FE; // PUSH 0xFE
-    if (cpu.cdtimer_to !== 1'b1) $error("cdtimer must be timed out");
-    if (cpu.irq !== 1'b1) $error("IRQ must be 1");
-    if (cpu.ien !== 1'b1) $error("IEN must be 1");
-    if (mem_addr !== `ADDR_WIDTH'h358) $error("mem_addr must be 0x358");
-  */
-
-  #11 // decode
-  #10 // exec
-  #10 // rdmem
-    rd_data <= 16'h7812; // IRET
+    rd_data <= 16'h7812; // 900: IRET
     if (~cpu.signals.phase_rdmem) $error("phase_rdmem must be 1");
     if (cpu.ien !== 1'b0) $error("IEN must be 0");
     if (mem_addr !== `ADDR_WIDTH'h900) $error("mem_addr must be 0x900");
     if (stack0 !== 16'h00FF) $error("stack0 must be 0x00FF");
+    irq <= 0;
 
   @(posedge cpu.load_insn)
   #11 // decode
   #10 // exec
   #10 // rdmem
-    rd_data <= 16'h80FE; // PUSH 0xFE
+    rd_data <= 16'h80FE; // 34e: PUSH 0xFE
     if (cpu.ien !== 1'b1) $error("IEN must be 1");
-    if (mem_addr !== `ADDR_WIDTH'h358) $error("mem_addr must be 0x358");
+    if (mem_addr !== `ADDR_WIDTH'h34e) $error("mem_addr must be 0x34e");
+
+  @(posedge cpu.load_insn)
+  #11 // decode
+  #10 // exec
+  #10 // rdmem
+    if (stack0 !== 16'h00FE) $error("stack0 must be 0x00FE");
 
   $finish;
 end
