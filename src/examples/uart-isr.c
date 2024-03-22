42d443d99acd27300279c07ae705eaf77ed649a2
uchan-nos <uchan@uchan.net>
2024-03-22 17:41:00 +0900

    UART 割り込みハンドラの挙動テストプログラムを追加

---
+++ src/examples/uart-isr.c
@@ -0,0 +1,21 @@
+int tim_count __attribute__((at(0x02)));
+int uart_data __attribute__((at(0x06)));
+int uart_flag __attribute__((at(0x08)));
+
+int recv_cnt;
+int recv_last;
+
+void _ISR() {
+  if (uart_flag & 1) {
+    recv_last = uart_data;
+    recv_cnt++;
+  }
+}
+
+int main() {
+  asm("push _ISR\n\t"
+      "isr");
+  uart_flag = 2; // 受信割り込みを有効化
+  while (recv_cnt < 16);
+  return recv_cnt;
+}
