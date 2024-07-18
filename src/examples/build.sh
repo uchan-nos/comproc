4c87bf983e76476762a0d71169678b966f9cabbc
uchan-nos <uchan@uchan.net>
2024-07-18 13:29:35 +0900

    build.sh に存在しないファイルを指定した際の誤動作防止

--- src/examples/build.sh
+++ src/examples/build.sh
@@ -12,6 +12,12 @@ asm=$base.s
 hex=$base.hex
 map=$base.map
 
+if [ ! -f "$src" ]
+then
+  echo "No such file: $src"
+  exit 1
+fi
+
 ../compiler/ucc -o $asm $src
 ../assembler/uasm -o $hex --map $map $asm
 
